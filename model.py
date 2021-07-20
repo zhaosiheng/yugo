2c68cc02e8cf2e3a3ee3a1d2cce728f30816d07f
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-07-20 10:49:58 +0800

    Update model.py

--- model.py
+++ model.py
@@ -102,8 +102,10 @@ class CombineGraph(Module):
         gama = torch.softmax(self.leakyrelu(e) * min(0.5 * pow(20 / 0.5, epoch / self.opt.E), 20), 1)        
         pai = gama * pos_emb
         pos_emb = pai.sum(1)
+        '''
         l2 = pai.pow(2).sum(-1).pow(0.5).sum(1) / pos_emb.pow(2).sum(-1).pow(0.5)
         pos_emb = l2.unsqueeze(-1).repeat(1,1,100) * pos_emb
+        '''
         self.gama = gama
         
         hs = hs.unsqueeze(-2).repeat(1, len, 1)
