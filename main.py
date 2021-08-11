f0a1ddbf1d7dcdb0b06aeb9b711727ceffa45da7
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-08-11 15:24:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main():
     elif opt.dataset == 'Tmall':
         num_node = 40728
         opt.n_iter = 1
-        opt.dropout_gcn = 0.4
+        opt.dropout_gcn = 0.3
         #opt.dropout_local = 0.5
         opt.dropout_local = 0.0
     else:
