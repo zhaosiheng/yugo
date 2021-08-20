9b817759097c71671bf9388be6d43182097db733
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-08-20 15:07:43 +0800

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -92,7 +92,7 @@ class GlobalAggregator(nn.Module):
         else:
             neighbor_vector = torch.mean(neighbor_vector, dim=2)
         # self_vectors = F.dropout(self_vectors, 0.5, training=self.training)
-        output = torch.cat([extra_vector, neighbor_vector], -1)
+        output = torch.cat([self_vectors, neighbor_vector], -1)
         output = F.dropout(output, self.dropout, training=self.training)
         output = torch.matmul(output, self.w_3)
 
