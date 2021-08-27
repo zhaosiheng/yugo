1663c502b0ba161ccf84f54a16e238b86b551f3d
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-08-27 15:56:10 +0800

    Update model.py

--- model.py
+++ model.py
@@ -43,8 +43,8 @@ class CombineGraph(Module):
         self.mine_w_1 = nn.Parameter(torch.Tensor(1, opt.pos_emb_len))
         self.mine_q_1 = nn.Parameter(torch.Tensor(1, opt.pos_emb_len))
         
-        self.Q = nn.Parameter(torch.Tensor(self.dim, (self.dim + self.opt.pos_num) / 2))
-        self.P = nn.Parameter(torch.Tensor((self.dim + self.opt.pos_num) / 2, opt.pos_num))
+        self.Q = nn.Parameter(torch.Tensor(self.dim, int((self.dim + self.opt.pos_num) / 2)))
+        self.P = nn.Parameter(torch.Tensor(int((self.dim + self.opt.pos_num) / 2), opt.pos_num))
         '''
         self.Q_4 = nn.Parameter(torch.Tensor(self.dim * 2 + 1, self.dim))
         self.P_4 = nn.Parameter(torch.Tensor(self.dim, 1))
