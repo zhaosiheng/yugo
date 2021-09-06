332c73031be5588b7216b7488cf3f9af24ece9d0
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-09-06 17:46:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def main():
         num_node = 43098
         #opt.n_iter = 1
         #opt.dropout_gcn = 0.2
-        opt.dropout_local = 0.0
+        #opt.dropout_local = 0.0
     elif opt.dataset == 'Nowplaying':
         num_node = 60417
         #opt.n_iter = 1
