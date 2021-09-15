243181492f66632870213c59d603c0b161c8b38f
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-09-15 10:35:25 +0800

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -25,7 +25,7 @@ class LocalAggregator(nn.Module):
         self.dropout = dropout
 
         self.hop = hop
-        self.range = hop + 1
+        self.range = hop
         self.a_list = torch.nn.ParameterList([nn.Parameter(torch.Tensor(self.dim, 1)) for i in range(self.range)])
 
         self.bias = nn.Parameter(torch.Tensor(self.dim))
