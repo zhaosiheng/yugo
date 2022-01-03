1865f3b19975fbf86d10104f4ce1297d00f6e91f
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-01-03 17:07:18 +0800

    Update model.py

--- model.py
+++ model.py
@@ -177,8 +177,8 @@ class CombineGraph(Module):
         beta = beta * mask
         select = torch.sum(beta * hidden, 1)
        
-        select = torch.matmul(torch.cat([select, zr.squeeze(-2)], -1), self.yogo)
-
+        #select = torch.matmul(torch.cat([select, zr.squeeze(-2)], -1), self.yogo)
+        print(zr.shape)
         
         b = self.embedding.weight[1:]  # n_nodes x latent_size
         scores = torch.matmul(select, b.transpose(1, 0))
