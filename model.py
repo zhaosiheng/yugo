fbc3e70c1a10263af3942db70b8f9be64fb043ec
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-10-28 16:45:13 +0800

    1645

--- model.py
+++ model.py
@@ -175,8 +175,8 @@ class CombineGraph(Module):
         #nh = pos_emb + hidden
         
         zr = hidden[torch.arange(batch_size).long(), torch.sum(mask, 1).squeeze().long() - 1]
-        print(zr.unsqueeze(-2).shape)
-        nh = torch.sigmoid(self.glu1(nh) + self.glu2(hs) + self.glu3(zr))
+        
+        nh = torch.sigmoid(self.glu1(nh) + self.glu2(hs) + self.glu3(zr.unsqueeze(-2)))
         beta = torch.matmul(nh, self.w_2)
         beta = beta * mask
         select = torch.sum(beta * hidden, 1)
