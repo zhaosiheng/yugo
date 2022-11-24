40b3a08ddf0ac905be1a763617f800ad8cd05d3a
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-11-24 22:02:32 +0800

    sl

--- model.py
+++ model.py
@@ -181,7 +181,7 @@ class CombineGraph(Module):
         beta = beta * mask
         select = torch.sum(beta * hidden, 1)
         #w[hl||hg]
-        select = torch.matmul(torch.cat([select, zr], -1), self.yogo)+select
+        #select = torch.matmul(torch.cat([select, zr], -1), self.yogo)+select
 
            
 
