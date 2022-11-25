ee882bf8f5ae3d7600da15f8abf4f7de265601e4
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-11-25 15:47:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ parser.add_argument('--validation', action='store_true', help='validation')
 parser.add_argument('--valid_portion', type=float, default=0.1, help='split the portion')
 parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
 parser.add_argument('--patience', type=int, default=3)
-parser.add_argument('--s_l', type=bool, default=True)
+parser.add_argument('--s_l', action='store_true', default=False)
 opt = parser.parse_args()
 
 
