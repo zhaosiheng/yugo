7bcfc06436687966839608004ebc1a4bf1acc659
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-11-27 14:37:53 +0800

    sl

--- aggregator.py
+++ aggregator.py
@@ -39,8 +39,8 @@ class SGCN(nn.Module):
 
         A = adj[:,1]/2 + adj[:,0]
         D = torch.sum(A, -1).diag_embed().rsqrt()
-         
-        print(D.shape)
+        
+        print(D[0])
 
         a_input = (h.repeat(1, 1, N).view(batch_size, N * N, self.dim)
                    * h.repeat(1, N, 1)).view(batch_size, N, N, self.dim)
