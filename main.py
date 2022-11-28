afb809b1a352dc5b2d328ac96626b43a038f7fa4
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-11-28 23:05:24 +0800

    sl

--- main.py
+++ main.py
@@ -66,8 +66,8 @@ def main():
     elif opt.dataset == 'Tmall':
         num_node = 40728
         opt.n_iter = 1
-        opt.dropout_gcn = 0.6
-        opt.dropout_local = 0.5
+        #opt.dropout_gcn = 0.6
+        #opt.dropout_local = 0.5
     else:
         num_node = 310
 
