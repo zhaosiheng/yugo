a9df34a44822b7f83e126d9cd32f8ee9c89868d0
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-11-29 14:10:37 +0800

    sl

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
     if opt.dataset == 'diginetica':
         num_node = 43098
         #opt.n_iter = 1
-        opt.dropout_gcn = 0.2
+        opt.dropout_gcn = 0.0
         opt.dropout_local = 0.0
     elif opt.dataset == 'Nowplaying':
         num_node = 60417
