119ad77c9b65fee927beb68ffb46b3aed323e68a
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-11-30 14:23:09 +0800

    sl

--- model.py
+++ model.py
@@ -235,10 +235,8 @@ class CombineGraph(Module):
         star_0 = torch.matmul(sum_item_emb, self.star_1)
         star_weight = torch.matmul(star_s, star_0.transpose(-2,-1)) / self.dim**0.5
         star_weight = torch.softmax(star_weight, -1)
-        
-        print(star_weight.shape)
         s_global = torch.sum(star_weight * h_global, -1)
-        
+        print(s_global.shape)
         output = h_local + s_global
 
         return output
