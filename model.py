31cb3c572e49e1520f9655f7a7a8a6f0bed20eaf
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2023-01-26 11:48:23 +0800

    0

--- model.py
+++ model.py
@@ -168,6 +168,7 @@ class CombineGraph(Module):
         hs = hs.unsqueeze(-2).repeat(1, len, 1)
         nh = torch.matmul(torch.cat([pos_emb, hidden], -1), self.w_1)
         nh = torch.tanh(nh)
+        print(nh.shape)
         #nh = pos_emb + hidden
         
         zr = hidden[torch.arange(batch_size).long(), torch.sum(mask, 1).squeeze().long() - 1]
