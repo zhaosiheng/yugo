4f5d3209001265b6e29b1d56f01ea1037183f312
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2023-01-26 13:51:16 +0800

    0

--- model.py
+++ model.py
@@ -176,7 +176,7 @@ class CombineGraph(Module):
         nh = torch.sigmoid(self.glu1(nh) + self.glu2(hs) + self.glu3(zr.unsqueeze(-2)))
         beta = torch.matmul(nh, self.w_2)
 
-        tmp = torch.tensor(1,dtype = torch.float).unsqueeze(0).unsqueeze(0).repeat(self.batch_size, 1, 1)
+        tmp = torch.tensor(1,dtype = torch.float).unsqueeze(0).unsqueeze(0).repeat(batch_size, 1, 1)
         tmp = trans_to_cuda(tmp)
         print(tmp.shape)
         print(mask.shape)
