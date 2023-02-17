c2d3d49e197056df8b346ecfe4cf833e72fe0718
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2023-02-17 15:05:38 +0800

    0

--- aggregator.py
+++ aggregator.py
@@ -135,3 +135,32 @@ class GlobalAggregator(nn.Module):
 
         output = self.act(output)
         return output #.unsqueeze(-2).repeat(1, seqs_len, 1)
+
+class GlobalAggregator_org(nn.Module):
+    def __init__(self, dim, dropout, act=torch.relu, name=None):
+        super(GlobalAggregator, self).__init__()
+        self.dropout = dropout
+        self.act = act
+        self.dim = dim
+
+        self.w_1 = nn.Parameter(torch.Tensor(self.dim + 1, self.dim))
+        self.w_2 = nn.Parameter(torch.Tensor(self.dim, 1))
+        self.w_3 = nn.Parameter(torch.Tensor(2 * self.dim, self.dim))
+        self.bias = nn.Parameter(torch.Tensor(self.dim))
+
+    def forward(self, self_vectors, neighbor_vector, batch_size, masks, neighbor_weight, extra_vector=None):
+        if extra_vector is not None:
+            alpha = torch.matmul(torch.cat([extra_vector.unsqueeze(2).repeat(1, 1, neighbor_vector.shape[2], 1)*neighbor_vector, neighbor_weight.unsqueeze(-1)], -1), self.w_1).squeeze(-1)
+            alpha = F.leaky_relu(alpha, negative_slope=0.2)
+            alpha = torch.matmul(alpha, self.w_2).squeeze(-1)
+            alpha = torch.softmax(alpha, -1).unsqueeze(-1)
+            neighbor_vector = torch.sum(alpha * neighbor_vector, dim=-2)
+        else:
+            neighbor_vector = torch.mean(neighbor_vector, dim=2)
+        # self_vectors = F.dropout(self_vectors, 0.5, training=self.training)
+        output = torch.cat([self_vectors, neighbor_vector], -1)
+        output = F.dropout(output, self.dropout, training=self.training)
+        output = torch.matmul(output, self.w_3)
+        output = output.view(batch_size, -1, self.dim)
+        output = self.act(output)
+        return output
